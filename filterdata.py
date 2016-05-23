8e07086b78bbfb58be715ccc35a58e0f40c35b1b
djx <djx@localhost.localdomain>
2016-05-23 10:20:28 +0800

    e

---
+++ filterdata.py
@@ -0,0 +1,22 @@
+from myxl import *
+
+#根据源数据过滤不符合条件的数据
+#source.xls为比对的源文件（其中的包含正确的数据）
+#mydata.xls为上报的需要被过滤的数据文件
+
+#获用于取比对的源数据列：[0]表示第一列
+srcs = get_data_cols('source.xls')[0]
+
+#获取需要被过滤的数据文件
+datass = get_data('mydata.xls')
+
+resdata = []
+for datarow in datass:
+     # 以下[1]为过滤依据列序号，从0开始
+    if isinstance(datarow[1],str) and datarow[1] in srcs:
+        resdata.append(datarow)
+    else:
+        print(datarow)
+
+#写入过滤后的数据        
+save_datas_xlsx('rightdata.xlsx',resdata)
